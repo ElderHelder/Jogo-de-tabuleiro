76a19f453f1e0f70a45139c5d285e0af72030b05
ElderHelder <hrasantos.cic@uesc.br>
2025-12-01 11:11:06 -0300

    Create cenário.c

---
+++ cenário.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*Primeiro crio uma arvore com galho esquerdo, direito e conteudo proprio */
+typedef struct tree {
+  	int info;
+  	struct tree *esq;
+ 	struct tree *dir;
+} arvore;
+/* Função que insere elemento na árvore, retorna o tipo arvore*/
+arvore *insere(arvore *raiz, int valor) {
+
+    // Árvore vazia, o novo nó será a raiz
+/*Alocando espaço e um endereço para a raiz, indicando que os galhos estão vazios, quando a arvore recursivamente chegar em um galho vazio, vai criar ele e seus galhos, dando valor e endereço*/
+    if (raiz == NULL) {
+        arvore *folha = (arvore*) malloc(sizeof(arvore));
+        folha->info = valor;
+        folha->dir = NULL;
+        folha->esq = NULL;
+
+        return folha;
+    }
+
+    if (valor > raiz->info)
+        raiz->dir = insere(raiz->dir, valor);
+    else
+        raiz->esq = insere(raiz->esq, valor);
+
+    return raiz;
+}
+
+// Função imprime elementos in-order
+void imprimeInorder(arvore *raiz) {
+    if (raiz == NULL)
+        return;
+/*imprime o último esquerdo e vai voltando, quando imprimir todas as esquerdas, vai para a direita e imprime toda a esquerda dele, imprimindo em ordem*/
+    imprimeInorder(raiz->esq);
+    printf("%d ", raiz->info);
+    imprimeInorder(raiz->dir);
+}
